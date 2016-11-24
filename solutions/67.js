3e1e27e0dbc515b22b63a845c3aef2386dcbdf85
SimpleAOB <simpleaob@live.com>
2016-11-23 21:07:41 -0500

    Update 67.js

--- solutions/67.js
+++ solutions/67.js
@@ -1,3 +1,3 @@
 var k = CryptoJS.SHA256("\x93\x39\x02\x49\x83\x02\x82\xf3\x23\xf8\xd3\x13\x37");
 var decrypted = CryptoJS.AES.decrypt("ob1xQz5ms9hRkPTx+ZHbVg==", CryptoJS.enc.Hex.parse(k.toString().substring(0,32)), { iv: CryptoJS.enc.Hex.parse(k.toString().substring(32,64)) });
-print.console(decrypted.toString());
+print.console(CryptoJS.enc.Utf8.stringify(decrypted));
